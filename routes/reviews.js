81a17c07c93183bf24095f713528b6754f51e6c8
Armeth <jwpro1984@gmail.com>
2023-06-19 18:01:29 -0400

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -28,7 +28,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review);
     await review.save();
     await campground.save();
-    req.flash('success', 'Review created!')
+    req.flash('success', 'Review created! Thank you!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
@@ -37,6 +37,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Campground review deleted');
     res.redirect(`/campgrounds/${id}`);
 }))
 
