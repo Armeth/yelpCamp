ee2dc58e79ae1b66c95db1e972b9d14ac1bd64cf
Armeth <jwpro1984@gmail.com>
2023-07-18 13:58:32 -0400

    Middleware for review authors

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 //expressRouter likes to keep ids separate --> thus mergeParams: true as an option
 const catchAsync = require('../utils/catchAsync');
-const {validateReview} = require('../middleware')
+const {validateReview, isLoggedIn, isReviewAuthor} = require('../middleware')
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -16,7 +17,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
     //this req.params can be deconstructed into const {id, reviewId} = req.params.
     const {id, reviewId} = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
